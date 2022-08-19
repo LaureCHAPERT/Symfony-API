1ddd1b80e93854e6371f9d2c74c57de7250f0c3b
LaureCHAPERT <laure.chapert@gmail.com>
2022-08-19 11:06:06 +0200

    method to get an user with id

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -32,4 +32,15 @@ class UserController extends AbstractController
         $response = $this->json($users, 200, []);
         return $response;
     }
+    /**
+     * @Route("/user/{id}", name="app_user_list", methods={"GET"})
+     */
+    public function user(UserRepository $userRepository, $id): Response
+    {
+        // Data recovery (Repository)
+        $user = $userRepository->find($id);
+        //Returns a JsonResponse that uses the serializer component if enabled, or json_encode
+        $response = $this->json($user, 200, []);
+        return $response;
+    }
 }
